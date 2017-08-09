bbf93adb879c91ff19fc297478b7e1bd0c839b4c
Aleksandr Shenshin <shenshin@me.com>
2017-08-09 03:16:37 +0300

    that was the lesson one

---
+++ views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  swoosh
+//
+//  Created by Aleksandr Shenshin on 09.08.17.
+//  Copyright © 2017 Aleksandr Shenshin. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
